3fd0ba2df1e67368c8f0c3ba4cd0a5479e188fe3
Deepak S B <deepak609sb@gmail.com>
2025-09-05 05:23:28 +0000

    increase the alert sourses donut chart size similar to MITRE ATT&CK Tech

--- src/components/dashboard/AlertSourcesChart.tsx
+++ src/components/dashboard/AlertSourcesChart.tsx
@@ -45,9 +45,9 @@ export function AlertSourcesChart({ className }: { className?: string }) {
         <CardTitle>Alert Sources</CardTitle>
         <CardDescription>Last 24 hours</CardDescription>
       </CardHeader>
-      <CardContent className="flex-1 flex items-center justify-center p-0">
-        <div className="relative w-full h-full">
-            <ChartContainer config={chartConfig} className="absolute inset-0">
+      <CardContent className="flex-1 flex flex-col items-center justify-center p-4">
+        <div className="relative w-full h-[250px] flex items-center justify-center">
+            <ChartContainer config={chartConfig} className="mx-auto aspect-square h-full">
             <PieChart>
                 <ChartTooltip
                 cursor={false}
